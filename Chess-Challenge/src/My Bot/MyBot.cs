4c10e978c87427c46468775b7ed49cae5ab2f702
Matthew Apple <mallgaier06@gmail.com>
2023-07-21 16:16:01 -0400

    First capture + promote

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,10 +1,27 @@
 ﻿using ChessChallenge.API;
+using System;
 
 public class MyBot : IChessBot
 {
+    /* Version stats
+     * 
+     * Initial Commit (moves[0]): +0, =21, -111
+     * First capture + promote: +1, =26, -72
+     */
     public Move Think(Board board, Timer timer)
     {
         Move[] moves = board.GetLegalMoves();
-        return moves[111111 % moves.Length];
+        Move bestMove = moves[0];
+        int bestMoveScore = int.MinValue;
+        for (int i = 0; i < moves.Length; ++i)
+        {
+            int moveScore = 0 + Convert.ToInt32(moves[i].IsCapture) + Convert.ToInt32(moves[i].IsPromotion);
+            if (moveScore > bestMoveScore)
+            {
+                bestMove = moves[i];
+            }
+        }
+
+        return bestMove;
     }
 }
