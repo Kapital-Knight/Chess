7af994bca49b28fab729f38441e1f97b76a35ee3
Matthew Apple <mallgaier06@gmail.com>
2023-07-21 17:44:08 -0400

    Weighted composite score (rank, promotion, and capture)

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -7,6 +7,7 @@ public class MyBot : IChessBot
      * 
      * Initial Commit (moves[0]): +0, =21, -111
      * First capture + promote: +1, =26, -72
+     * Weighted composite score (rank, promotion, and capture): +4, =112, -18
      */
     public Move Think(Board board, Timer timer)
     {
@@ -15,9 +16,18 @@ public class MyBot : IChessBot
         int bestMoveScore = int.MinValue;
         for (int i = 0; i < moves.Length; ++i)
         {
-            int moveScore = 0 + Convert.ToInt32(moves[i].IsCapture) + Convert.ToInt32(moves[i].IsPromotion);
+            Move move = moves[i];
+            int pawnRankGain = (move.MovePieceType == PieceType.Pawn)?((board.IsWhiteToMove)?1:-1) * (move.TargetSquare.Rank - move.StartSquare.Rank):0;
+            int promotionGain = ((int)move.PromotionPieceType - 1);
+            int captureGain = (int)move.CapturePieceType;
+            // Type: None = 0, Pawn = 1, Knight = 2, Bishop = 3, Rook = 4, Queen = 5, King = 6
+            int moveScore = 0
+                + 1 * pawnRankGain 
+                + 1 * captureGain 
+                + 2 * promotionGain;
             if (moveScore > bestMoveScore)
             {
+                bestMoveScore = moveScore;
                 bestMove = moves[i];
             }
         }
