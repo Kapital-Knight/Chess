cdc6e8312472c786b06cd4950ccc3640a1749ac6
Matthew Apple <mallgaier06@gmail.com>
2023-07-21 22:59:22 -0400

    Weighted composite scores with official piece values

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -9,6 +9,9 @@ public class MyBot : IChessBot
      * First capture + promote: +1, =26, -72
      * Weighted composite score (rank, promotion, and capture): +4, =112, -18
      */
+
+    // Piece values. None 0, Pawn 1, Knight 3, Bishop 3, Rook 5, Queen 9, King 100
+    int[] pieceValue = { 0, 1, 3, 3, 5, 9, 100 }; 
     public Move Think(Board board, Timer timer)
     {
         Move[] moves = board.GetLegalMoves();
@@ -17,14 +20,18 @@ public class MyBot : IChessBot
         for (int i = 0; i < moves.Length; ++i)
         {
             Move move = moves[i];
+            // range: 0 - 2
             int pawnRankGain = (move.MovePieceType == PieceType.Pawn)?((board.IsWhiteToMove)?1:-1) * (move.TargetSquare.Rank - move.StartSquare.Rank):0;
-            int promotionGain = ((int)move.PromotionPieceType - 1);
-            int captureGain = (int)move.CapturePieceType;
-            // Type: None = 0, Pawn = 1, Knight = 2, Bishop = 3, Rook = 4, Queen = 5, King = 6
-            int moveScore = 0
-                + 1 * pawnRankGain 
-                + 1 * captureGain 
-                + 2 * promotionGain;
+            // range: 0 - 4
+            int promotionGain = pieceValue[(int)move.PromotionPieceType] - 1;
+            // range: 0 - 6
+            int captureGain = pieceValue[(int)move.CapturePieceType];
+            // Next, punish moving into a pawn's range and reward moving out of it
+            
+            int moveScore
+                = 10 * pawnRankGain
+                + 20 * promotionGain
+                + 10 * captureGain;
             if (moveScore > bestMoveScore)
             {
                 bestMoveScore = moveScore;
